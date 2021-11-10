04a0d7bea473bd7b6b3b209e0f9c1ed922bb3ffd
whitehat-G6 <88480121+WHJR-G6@users.noreply.github.com>
2021-11-10 12:02:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,58 +1,62 @@
-var jack,jack2;
-var platform,platform2;
-var jack_image, platform_image;
-var platform1,platform2,platform3,platform4,platform5,platform6;
-
-function preload(){
-  jack_image=loadImage("images/jack2.png")
-  
- 
-platform1 = loadImage("images/platform1.png")
-platform2 = loadImage("images/platform2.png")
-platform3 = loadImage("images/platform3.png")
-platform4 = loadImage("images/platform4.png")
-platform5 = loadImage("images/platform5.png")
-platform6 = loadImage("images/platform6.png")
-
-}
-
-
 function setup() {
   createCanvas(500, 500);
-  jack = createSprite(250,300,20,50);
-  jack.addImage("jack",jack_image)
+ //bg = createSprite(250,250,500,500);
+  //bg.addImage(bgImg)
+  //bg.scale =0.2;
+  player = createSprite(250,300,20,50);
+  player.addImage("player",jack_image)
 
   platform = createSprite(250,350,100,10);
   platform.addImage("platform", platform3)
-  platform.scale=0.1
-  
+  platform.scale=0.1;
+ platformGroups = new Group(); 
 }
 
 function draw() {
-  background('#F2F5B6');
-     
+  background(0);
+
+  //calculate and display score
+    score = Math.round(frameCount/10);
+
+  // printing score
   
+    
   if(keyDown("up"))
   {
-    jack.velocityY=-10
+    player.velocityY=-10
+    //jumpSound1.play()
+  }
+
+  if(keyDown("left"))
+  {
+    player.x = player.x - 7;
+    //leftSound.play()
   }
 
 
+  player.velocityY = player.velocityY +0.8;
 
-  jack.velocityY = jack.velocityY +0.8;
-  jack.collide(platform)
-
+  player.collide(platform);
+  
+  
+  player.collide(platformGroups);
+  
+  
   spawnPlatform();
 
   drawSprites();
+  textSize(20);
+  fill(255);
+  text("Score " + score , 350,50);
 }
 
 function spawnPlatform(){
-
-  if(frameCount % 60 ===0){
-  
+  if(frameCount % 40 ===0){
+    class 13
+    platform.destroy();
+   
     var platforms = createSprite(250,0,50,50);
-  platforms.velocityY = 4;
+  platforms.velocityY = 5;
 
   var r = Math.round(random(1,6))
   switch (r){
@@ -69,8 +73,10 @@ function spawnPlatform(){
     case 6 : platforms.addImage(platform6);
     break;
   }
-  platforms.collide(jack);
+ 
   platforms.scale = 0.1;
+
+  platformGroups.add(platforms);
 
   platforms.x = random(0,500);
   }
